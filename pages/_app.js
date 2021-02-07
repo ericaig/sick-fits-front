260fd43864d29c8cb1678b6e441ac37fb6f4103b
Eric Aig <eric@kompini.com>
2021-02-07 18:29:57 +0100

    Initial commit @video_23

---
+++ pages/_app.js
@@ -0,0 +1,44 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+import { ApolloProvider } from '@apollo/client';
+import withData from '../lib/withData';
+
+Router.events.on('routeChangeStart', NProgress.start);
+Router.events.on('routeChangeComplete', NProgress.done);
+Router.events.on('routeChangeError', NProgress.done);
+
+function MyApp({ Component, pageProps, apollo }) {
+  /**
+   * apollo - comes from the `withData` fnc
+   */
+
+  return (
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
+}
+
+/**
+ * Boilerplate
+ * What this does is that if any of the pages has `getInitialProps`
+ * on them, then we are going to wait for it to completely execute or
+ * usually what for the data fetching to complete
+ */
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  let pageProps = {}
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx)
+  }
+
+  pageProps.query = ctx.query
+
+  return { pageProps }
+}
+
+export default withData(MyApp)
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+/**
+ * if you want to make a route the same as something else, just do this
+ * In this case, we are making home page same as products page...
+ */
+
+// import ProductsPage from './products';
+// export default ProductsPage
+
+//  same as
+export { default } from "./products";
